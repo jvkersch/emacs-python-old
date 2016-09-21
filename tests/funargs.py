5c9c3aaa9592291b3387328a511ed00dcf6bcdc5
Joris Vankerschaver <jvankerschaver@enthought.com>
2016-09-21 17:47:38 +0100

    ENH: Determine types directly from emacs.

--- tests/funargs.py
+++ tests/funargs.py
@@ -1,16 +1,16 @@
 from pymacs_utils import expose
 
 
-@expose(args=(int, int))
+@expose
 def py_add_int(x, y):
     return x + y
 
 
-@expose(args=(float, float))
+@expose
 def py_add_float(x, y):
     return x + y
 
 
-@expose(args=(str,))
+@expose
 def py_len_str(s):
     return len(s)
