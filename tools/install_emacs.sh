c71bb0bca08e1589e0a44425a9cfa2875c3dee74
Joris Vankerschaver <jvankerschaver@enthought.com>
2016-09-24 20:29:58 +0100

    foo

--- tools/install_emacs.sh
+++ tools/install_emacs.sh
@@ -6,9 +6,14 @@ set -e
 
 EMACS_INSTALL_DIR="$HOME/emacs"
 
+echo "looking in $EMACS_INSTALL_DIR"
+
 mkdir -p "$EMACS_INSTALL_DIR"
 if [ -f "$EMACS_INSTALL_DIR/bin/emacs" ]; then
+    echo "it exists"
     exit 0
+else
+    echo "it doesn't exist"
 fi
 
 wget http://ftp.gnu.org/gnu/emacs/emacs-25.1.tar.gz
